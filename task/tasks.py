8833924ecacbf382c59cfd95d8d1962d811b2d56
Zhenxuan Xu <Zhenxuan.Xu@ygomi.com>
2018-10-10 17:14:29 +0800

    add task touch

--- task/tasks.py
+++ task/tasks.py
@@ -8,17 +8,13 @@
 from celery import task
 import time
 from django_celery_results.models import TaskResult
-from .run_offlineSLAM import Run
+import os
 
 
 @task
 def print_task(name):
     print("hello {}".format(name))
     time.sleep(30)
+    os.system("touch ~/this_is_django_celery_test.txt")
     print("task over")
 
-
-@task
-def run_slam():
-    run = Run()
-    run._start()
