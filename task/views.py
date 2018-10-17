5f6603444ecc4d4bbab49e5b052cd3e7a1f0a80a
Zhenxuan Xu <Zhenxuan.Xu@ygomi.com>
2018-10-17 12:18:56 +0800

    add task area

--- task/views.py
+++ task/views.py
@@ -35,7 +35,8 @@ def submitted(request):
                'common-sam': request.POST['common-sam'], 'algo_common-sam': request.POST['algo_common-sam']}
     task = Task(tester=request.user, mode=request.POST['select_mode'], branch=branchs, area=request.POST.getlist('check_box_list'), status="Waiting")
     task.save()
-    run_slam.delay("milford", str(request.user), task.id)
+    for area in task.area:
+        run_slam.delay(str(area), str(request.user), task.id)
 
     # result = print_task.delay("xu")
     # print(result.task_id)
