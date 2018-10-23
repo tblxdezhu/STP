7e93632e828abe6b1aa62f38a9b43df611591208
Zhenxuan Xu <Zhenxuan.Xu@ygomi.com>
2018-10-23 18:14:27 +0800

    add get json for branchs

--- task/compile_code.py
+++ task/compile_code.py
@@ -14,20 +14,24 @@ class Compile_code(object):
 
     compile_info=dict()
 
-    def __init__(self,task_id,common="master",algo_common="master",algo_vehicle_offlineslam="master",
-                 common_sam="master",algo_common_sam="master",algo_sam="master",algo_sam_commit="",
-                 algo_offlineSLAM_commit=""
-                 ):
+    def __init__(self,**kwarg):
+
+        if kwarg["is_sam"]:
+            self.compile_info["is_sam"]=True
+            self.compile_info["algorithm_sam"]=kwarg["algorithm_sam"]
+        else:
+            self.compile_info["is_sam"] = False
+            self.compile_info["algorithm_vehicle_offlineslam"]=kwarg["algorithm_vehicle_offlineslam"]
+
+        if kwarg.__contains__("commit_point") and kwarg["commit_point"]:
+            self.compile_info["commit_point"] = kwarg["commit_point"]
+        else:
+            self.compile_info["commit_point"]=""
+
         self.compile_info["code_path"]= "/home/roaddb/test_code/"
-        self.compile_info["common"] = common
-        self.compile_info["algo_common"] = algo_common
-        self.compile_info["algo_vehicle_offlineslam"] = algo_vehicle_offlineslam
-        self.compile_info["common-sam"] = common_sam
-        self.compile_info["algo_common-sam"] = algo_common_sam
-        self.compile_info["algo_sam"] = algo_sam
-        self.compile_info["algo_sam_commit"]=algo_sam_commit
-        self.compile_info["algo_offlineSLAM_commit"]=algo_offlineSLAM_commit
-        self.compile_info["task_id"]=task_id
+        self.compile_info["common"] = kwarg["common"]
+        self.compile_info["algorithm_common"] = kwarg["algorithm_common"]
+
 
         self.compile_info["stash_common"]="ssh://git@stash.ygomi.com:7999/rc/common.git"
         self.compile_info["stash_algo_common"]="ssh://git@stash.ygomi.com:7999/rc/algorithm_common.git"
@@ -52,7 +56,6 @@ class Compile_code(object):
             client = paramiko.SSHClient()
             client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             client.connect(sys_ip, 22, username=username, password=password, compress=True)
-            #print(sys_ip, 22, username, password)
             cmd=""
             if cmds["git_commit_point"]:
                 cmd=cmds["clear_repo_cmd"]+";"+cmds["git_clone"]+";"+cmds["git_checkout_cmd"]+";"+\
@@ -61,22 +64,10 @@ class Compile_code(object):
                 cmd=cmds["clear_repo_cmd"]+";"+cmds["git_clone"]+";"+cmds["git_checkout_cmd"]+";"+\
                     cmds["compile_cmd"]
 
-            print(cmd)
-            # stdin_clear,stdout_clear,stderr_clear=client.exec_command(command=cmds["clear_repo_cmd"])
-            # results["clear_repo_cmd"]=stdout_clear
-            # results["sdterr"]=stderr_clear
-            #
-            # stdin_clone, stdout_clone, stderr_clone = client.exec_command(command=cmds["git_clone"])
-            # results["git_clone"] = stdout_clone
-            # results["sdterr"]+=stderr_clone
-            #
-            # stdin_checkout, stdout_checkout, stderr_checkout = client.exec_command(command=cmds["git_checkout_cmd"])
-            # results["git_checkout_cmd"] = stdout_checkout
-            # results["sdterr"] += stderr_checkout
-
             stdin_compile, stdout_compile, stderr_compile = client.exec_command(command=cmd)
             results["compile_cmd"] = stdout_compile.channel.recv_exit_status()
             results["compile_cmd_detail"] = stdout_compile.readlines()
+            print(results["compile_cmd_detail"])
 
             return results
         except:
@@ -86,39 +77,32 @@ class Compile_code(object):
         finally:
             client.close()
 
-    def __compile_common(self,is_slam,evn_ip):
+    def __compile_common(self,evn_ip):
 
-        cmds=None
-        if is_slam:
-            cmds=self.__compile_cmds__(self.compile_info["code_path"],
-                              "common",
-                              self.compile_info["stash_common"],
-                              self.compile_info["common"],"")
-        else:
-            cmds = self.__compile_cmds__(self.compile_info["code_path"],
-                                     "common",
-                                     self.compile_info["stash_common"],
-                                     self.compile_info["common-sam"],"")
+        cmds=self.__compile_cmds__(self.compile_info["code_path"],
+                          "common",
+                          self.compile_info["stash_common"],
+                          self.compile_info["common"],"")
 
         compile_result=self.__ssh_run_cmd__(evn_ip,"roaddb","test1234",cmds)
 
         return compile_result
 
-    def __compile_algo_common(self,is_slam,evn_ip):
+    def __compile_algo_common(self,is_sam,evn_ip):
 
         cmds=None
-        if is_slam:
+        if not is_sam:
             cmds=self.__compile_cmds__(self.compile_info["code_path"],
                                        "algorithm_common",
                                        self.compile_info["stash_algo_common"],
-                                       self.compile_info["algo_common"],"",
-                                       "g")
+                                       self.compile_info["algorithm_common"],"",
+                                       "a")
 
         else:
             cmds=self.__compile_cmds__(self.compile_info["code_path"],
                                        "algorithm_common",
                                        self.compile_info["stash_algo_common"],
-                                       self.compile_info["algo_common-sam"],"",
+                                       self.compile_info["algorithm_common"],"",
                                        "g")
 
         compile_result=self.__ssh_run_cmd__(evn_ip,"roaddb","test1234",cmds)
@@ -130,8 +114,8 @@ class Compile_code(object):
         cmds=self.__compile_cmds__(self.compile_info["code_path"],
                                    "algorithm_vehicle_offlineslam",
                                    self.compile_info["stash_algo_vehicle_offlineslam"],
-                                   self.compile_info["algo_vehicle_offlineslam"],
-                                   self.compile_info["algo_offlineSLAM_commit"],
+                                   self.compile_info["algorithm_vehicle_offlineslam"],
+                                   self.compile_info["commit_point"],
                                    "aes")
         #print(cmds)
         compile_result=self.__ssh_run_cmd__(evn_ip,"roaddb","test1234",cmds)
@@ -141,34 +125,30 @@ class Compile_code(object):
         cmds=self.__compile_cmds__(self.compile_info["code_path"],
                                    "algorithm_sam",
                                    self.compile_info["stash_algo_sam"],
-                                   self.compile_info["algo_common-sam"],
-                                   self.compile_info["algo_sam_commit"],
+                                   self.compile_info["algorithm_sam"],
+                                   self.compile_info["commit_point"],
                                     "g")
         compile_result=self.__ssh_run_cmd__(evn_ip,"roaddb","test1234",cmds)
         return compile_result
 
-    def run_compile(self,is_slam,env_ip):
+    def run_compile(self,is_sam,env_ip):
 
 
-        result_common=self.__compile_common(is_slam,env_ip)
-        print(result_common)
-        # for item in result_common:
-        #     print(item)
-        result_algo_common=self.__compile_algo_common(is_slam,env_ip)
-        print(result_algo_common)
+        result_common=self.__compile_common(env_ip)
+        result_algo_common=self.__compile_algo_common(is_sam,env_ip)
         result_algo=""
-        if is_slam:
+        print(is_sam)
+        if not is_sam:
             result_algo=self.__compile_algo_offlineslam(env_ip)
         else:
             result_algo=self.__compile_algo_sam(env_ip)
 
-        print(result_algo)
-
 if __name__=="__main__":
 
 
-    obj_compile=Compile_code(1,common="feature/RDB-28325-offline-slam-developing",
-                             algo_common="feature/RDB-28325-offline-slam-developing",
-                             algo_vehicle_offlineslam="feature/RDB-28325-offline-slam-developing"
+    obj_compile=Compile_code(common="feature/RDB-28325-offline-slam-developing",
+                             algorithm_common="feature/RDB-28325-offline-slam-developing",
+                             algorithm_vehicle_offlineslam="feature/RDB-28325-offline-slam-developing",
+                             is_sam=False
                              )
     obj_compile.run_compile(False,"10.69.142.16")
