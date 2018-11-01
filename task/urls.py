7d2bff776ae025ec98f67e83ef5470b11332424f
Zhenxuan Xu <Zhenxuan.Xu@ygomi.com>
2018-11-01 14:45:37 +0800

    add task urls

--- task/urls.py
+++ task/urls.py
@@ -7,10 +7,13 @@
 from django.conf.urls import include, url
 from task import views
 
+app_name = "task"
+
 urlpatterns = [
     url(r'^$', views.test),
     url(r'^submitted$', views.submitted),
     url(r'^(?P<task_id>[0-9]{1,5})/$', views.task_process, name='task_id'),
-    url(r'^(?P<task_id>[0-9]{1,5})/status$', views._get_task_status)
+    url(r'^(?P<task_id>[0-9]{1,5})/status$', views._get_task_status),
+    url(r'^(?P<task_id>[0-9]{1,5})/download$', views.download_file, name='download')
 
 ]
