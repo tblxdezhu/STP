5add0b5a1ffab54326e725ae8d7c3f0d351f12c4
Zhenxuan Xu <Zhenxuan.Xu@ygomi.com>
2018-11-15 16:56:26 +0800

    test new work flow

--- task/build_config.py
+++ task/build_config.py
@@ -5,7 +5,7 @@
 # @File    : build_config.py
 # @Software: Pycharm professional
 
-code_path = "/media/psf/Untitled/Auto_test_SLAM/envs/env0"
+code_path = "/data1/stp_resources/env/source"
 # code_path = "/home/roaddb/source"
 stash_core_common = "ssh://git@stash.ygomi.com:7999/rc/common.git"
 stash_algo_common = "ssh://git@stash.ygomi.com:7999/rc/algorithm_common.git"
