8f770036ea72262d5cb0952ab99b53e75c967573
Zhenxuan Xu <Zhenxuan.Xu@ygomi.com>
2018-11-30 11:08:39 +0800

    add collect_static folder

--- STP2/settings.py
+++ STP2/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static/')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
     os.path.join(BASE_DIR, 'test/'),
