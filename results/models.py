2cdd8224bb7cbd5dbbab131bb7013a694387198c
Zhenxuan Xu <Zhenxuan.Xu@ygomi.com>
2018-12-04 18:29:19 +0800

    test model manager

--- results/models.py
+++ results/models.py
@@ -7,6 +7,9 @@ from django.db import models
 # Create your models here.
 
 class ResultsManager(models.Manager):
+    def show_task_id(self):
+        return [i for i in self.values_list('task_id').distinct()]
+
     def total(self, task_id, keyword):
         return sum([float(i[0]) for i in self.filter(task_id=task_id).values_list(keyword)])
 
