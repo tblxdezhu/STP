3785fc964e62d3d27072ac7858daa360454c738e
Zhenxuan Xu <Zhenxuan.Xu@ygomi.com>
2018-12-10 18:41:40 +0800

    test ret

--- task/views.py
+++ task/views.py
@@ -247,6 +247,7 @@ def _get_task_kml(request, task_id, area):
     #                                           'center_data': "hahahahah",
     #                                           'kmls_data': kmls_data, 'myechart2': myechart1, 'script_list': script_list})
     # print("content", content)
+    print(center_data[list(center_data.keys())[0]])
     return JsonResponse({
         'area': area,
         'center_data': eval(center_data[list(center_data.keys())[0]])
