bf30fd30b5b3cffe2cd96356fc14602d00abcfa5
Zhenxuan Xu <Zhenxuan.Xu@ygomi.com>
2018-12-10 15:46:21 +0800

    test ret

--- task/views.py
+++ task/views.py
@@ -222,6 +222,7 @@ def _get_task_kml(request, task_id, area):
     # return JsonResponse({'task_id': task_id, 'area': area, 'center_data': center_data[list(center_data.keys())[0]]})
 
     print(list(center_data.keys()))
+    print(center_data[list(center_data.keys())[0]])
     kmls_data = []
     for k in get_all_kmls(os.path.join(task.output_path, area)):
         for key in sorted(data[k].keys()):
