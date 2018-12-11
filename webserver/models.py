9241769f55626d80800badc39cd014d77fb41ed2
Zhenxuan Xu <Zhenxuan.Xu@ygomi.com>
2018-12-11 15:16:50 +0800

    add ip for machine

--- webserver/models.py
+++ webserver/models.py
@@ -4,13 +4,14 @@ from django.db import models
 # Create your models here.
 
 class Machine(models.Model):
-    machine_id = models.CharField(max_length=50)
+    ip = models.CharField(max_length=20)
+    machine_id = models.CharField(max_length=50, blank=True)
     process_num = models.CharField(max_length=10)
     code_path = models.CharField(max_length=100)
     output_path = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.machine_id
+        return self.ip
 
 
 class Data(models.Model):
