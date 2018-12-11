856fd51815fb4cbaac3aa8d9c114097e34070164
Zhenxuan Xu <Zhenxuan.Xu@ygomi.com>
2018-12-11 15:50:52 +0800

    add machineadmin

--- webserver/admin.py
+++ webserver/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from .models import Machine, Data
 
-# Register your models here.
 
-admin.site.register(Machine)
-admin.site.register(Data)
+# Register your models here.
 
 
 class MachineAdmin(admin.ModelAdmin):
     list_display = {'ip', 'process_num', 'output_path'}
     search_fields = {'ip'}
+
+
+admin.site.register(Machine, MachineAdmin)
+admin.site.register(Data)
