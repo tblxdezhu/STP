2dfe54744db943a9f19a4e0ebf160015e217f222
Zhenxuan Xu <Zhenxuan.Xu@ygomi.com>
2018-12-11 15:54:50 +0800

    add machineadmin

--- webserver/admin.py
+++ webserver/admin.py
@@ -6,8 +6,8 @@ from .models import Machine, Data
 
 
 class MachineAdmin(admin.ModelAdmin):
-    list_display = ('ip', 'machine_id', 'process_num', 'output_path')
-    search_fields = {'ip'}
+    list_display = ('ip', 'machine_id', 'process_num', 'output_path',)
+    search_fields = ('ip',)
 
 
 admin.site.register(Machine, MachineAdmin)
