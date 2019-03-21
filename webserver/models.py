9c8cafa39e8ec7cbc61cd6880161d7036b89d90a
Zhenxuan Xu <Zhenxuan.Xu@ygomi.com>
2019-03-21 19:45:06 +0800

    create new table

--- webserver/models.py
+++ webserver/models.py
@@ -6,6 +6,7 @@ class DataManager(models.Manager):
     def all_areas(self):
         return [str(i[0]) for i in self.values_list('area')]
 
+
 class Machine(models.Model):
     ip = models.CharField(max_length=20, default="127.0.0.1")
     username = models.CharField(max_length=50, default="roaddb")
@@ -13,13 +14,12 @@ class Machine(models.Model):
     port = models.IntegerField(default=22)
     machine_id = models.CharField(max_length=50)
     process_num = models.IntegerField()
-    code_path = models.CharField(max_length=100)
+    code_path = models.CharField(max_length=100, default="/home/roaddb/source/core")
     output_path = models.CharField(max_length=100)
-    data_path = models.CharField(max_length=100, default="/media/psf/Untitled/SLAM_TEST/auto_test_large")
+    data_path = models.CharField(max_length=100)
 
     def __str__(self):
         return self.ip
-
 
 
 class Data(models.Model):
