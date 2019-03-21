126e926e9241cfcc4a803831d39f3cf0a1190ad2
Zhenxuan Xu <Zhenxuan.Xu@ygomi.com>
2019-03-21 15:04:06 +0800

    test repair kml

--- task/google_earth_related.py
+++ task/google_earth_related.py
@@ -69,6 +69,7 @@ def get_all_kmls(path):
     print("i am in func get_all_kmls")
     try:
         for root, dirs, files in os.walk(path):
+            print("files:",files)
             for file in files:
                 print("file in folder:",file)
                 # if file.endswith("final_pose.kml") or file.endswith("pre_process_gps.kml"):
